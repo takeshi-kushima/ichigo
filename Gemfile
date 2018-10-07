9e1b2848657f49f1ea2926469a102a0796f278dc
Takeshi Kushima <yukicha_0704@yahoo.co.jp>
2018-10-07 09:15:55 +0000

    環境変数設定

--- Gemfile
+++ Gemfile
@@ -45,6 +45,7 @@ gem 'carrierwave'
 gem 'will_paginate', '~>3.1.6'
 gem 'bootstrap-will_paginate', '~>1.0.0'
 gem 'faker','1.7.3'
+gem 'rails_12factor'
 
 group :development, :test do
   # Call 'byebug' anywhere in the code to stop execution and get a debugger console
--- config/environments/production.rb
+++ config/environments/production.rb
@@ -94,8 +94,8 @@ Rails.application.configure do
   
   config.action_mailer.raise_delivery_errors = true
   config.action_mailer.delivery_method = :smtp
-  host = 'otetsudainetto.herokuapp.com'
-  config.action_mailer.default_url_options = { host: otetsudainetto.herokuapp.com }
+  #host = 'otetsudainetto'
+  #config.action_mailer.default_url_options = { host: otetsudainetto.herokuapp.com }
   ActionMailer::Base.smtp_settings = {
     :address        => 'smtp.sendgrid.net',
     :port           => '587',
