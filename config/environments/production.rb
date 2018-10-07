73c994f4ff5f4819f3c51b28a5a8e13104ad3b13
Takeshi Kushima <yukicha_0704@yahoo.co.jp>
2018-10-07 14:44:48 +0000

    host先設定

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -94,8 +94,8 @@ Rails.application.configure do
   
   config.action_mailer.raise_delivery_errors = true
   config.action_mailer.delivery_method = :smtp
-  #host = 'otetsudainetto'
-  #config.action_mailer.default_url_options = { host: otetsudainetto.herokuapp.com }
+  host = 'polar-hollows-53728'
+  config.action_mailer.default_url_options = { host: polar-hollows-53728.herokuapp.com }
   ActionMailer::Base.smtp_settings = {
     :address        => 'smtp.sendgrid.net',
     :port           => '587',
